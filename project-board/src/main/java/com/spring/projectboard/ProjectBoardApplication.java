6c9cff63c3ab1ffcfcb24ae34aa639a04c3b6ccc
MoonStar <moonsung0331@gmail.com>
2023-03-02 15:46:25 +0900

    배포 테스트를 위한 구현

--- project-board/src/main/java/com/spring/projectboard/ProjectBoardApplication.java
+++ project-board/src/main/java/com/spring/projectboard/ProjectBoardApplication.java
@@ -2,11 +2,18 @@ package com.spring.projectboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @ConfigurationPropertiesScan
 @SpringBootApplication
-public class ProjectBoardApplication {
+public class ProjectBoardApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ProjectBoardApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectBoardApplication.class, args);
